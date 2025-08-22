c4e57dcd2dfbc5028d5207a4e10f2c7b030a8465
Sula249 <104200489+Sula249@users.noreply.github.com>
2025-08-23 01:52:31 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,4 @@
 // config.js
-const API_KEY = "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";
-const API_URL = "https://openrouter.ai/api/v1/chat/completions";
-const MODEL = "mistralai/mistral-7b-instruct:free";
-
-
+export const API_KEY = "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";
+export const API_URL = "https://openrouter.ai/api/v1/completions";
+export const MODEL = "mistralai/mistral-7b-instruct:free";
