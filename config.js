10bbb59c59e1698e1efdd8ed208e728d5a5469ec
Sula249 <104200489+Sula249@users.noreply.github.com>
2025-08-23 00:26:15 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 const API_KEY = "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";
-const API_URL = "https://sula249.github.io/prompt/";
+const API_URL = "https://sula249.github.io/prompt/api";
 const MODEL = "openai/gpt-4o-mini";
