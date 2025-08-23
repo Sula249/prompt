c0d9b739c704c39d80c3636005f500869798b988
Sula249 <104200489+Sula249@users.noreply.github.com>
2025-08-24 03:05:02 +0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // Настройки APIs
 const API_KEY = "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";
 const API_URL = "https://openrouter.ai/api/v1/chat/completions"; // Правильный endpoint для chat моделей
-const MODEL = "deepseek/deepseek-chat-v3.1"; // Модель OpenRouter
+const MODEL = "deepseek/deepseek-r1-0528-qwen3-8b:free"; // Модель OpenRouter
